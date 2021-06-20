fb7a211c093c169b8d4c9a95e3ad73f7d3abb7b9
k-shinn <k.shinn0721@gmail.com>
2021-06-20 14:54:51 +0900

    first commit

---
+++ app/src/main/java/com/kei/compose_pathway/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.kei.compose_pathway
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kei.compose_pathway.ui.theme.ComposePathwayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePathwayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposePathwayTheme {
+        Greeting("Android")
+    }
+}
