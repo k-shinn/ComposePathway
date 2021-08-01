46b89438df3798bbcca7d8e140a148813e38297b
k-shinn <k.shinn0721@gmail.com>
2021-08-01 17:01:52 +0900

    11. Intrinsics, finish

--- app/src/main/java/com/kei/compose_pathway/MainActivity.kt
+++ app/src/main/java/com/kei/compose_pathway/MainActivity.kt
@@ -8,10 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -38,6 +35,31 @@ class MainActivity : ComponentActivity() {
                 BodyContent()
             }
         }
+    }
+}
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            text = text1,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+        Divider(
+            color = Color.Black, modifier = Modifier
+                .fillMaxHeight()
+                .width(1.dp)
+        )
+        Text(
+            text = text2,
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
     }
 }
 
@@ -342,6 +364,9 @@ fun DefaultPreview() {
 //        Chip(text = "Hi there")
 //        ConstraintLayoutContent()
 //        LargeConstraintLayout()
-        DecoupledConstraintLayout()
+//        DecoupledConstraintLayout()
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
     }
 }
